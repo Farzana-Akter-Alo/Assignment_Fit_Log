98d2961cf8f44ac331d448b0bc77295471a9fdfd
Farzana Akter Alo <mstfarzana161@gmail.com>
2026-09-25 22:31:42 +0600

    added footer

--- app/Footer/page.tsx
+++ app/Footer/page.tsx
@@ -1,7 +1,26 @@
+import Image from "next/image";
+import FooterImg from "../assets/logo.png";
+
 export default function Footer() {
   return (
-    <div>
-      <h1>this is footer</h1>
-    </div>
+    <footer className="bg-black py-5">
+      <div className="container mx-auto">
+        <div className="flex items-center justify-between ">
+          <div className="flex gap-2 items-center">
+            <Image
+              src={FooterImg}
+              alt="Logo"
+              width={30}
+              height={20}
+              className="-rotate-225"
+            />
+            <p className="text-white font-bold text-lg">FITLOG</p>
+          </div>
+          <p className="text-gray-300 text-sm">
+            © 2026 FitLog — Workout Library. Train hard, log honest.
+          </p>
+        </div>
+      </div>
+    </footer>
   );
 }
